bcd429971f022beafb1c658278b361d49d041057
aktaion-x@aktaion <aktaion-x@proton.me>
2023-12-28 11:12:48 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/header'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className=''>
+      <Header />
+    </main>
+  )
+}
